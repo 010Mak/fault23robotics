b14550549916ddaa4bb7f3bff7473c9f11271d5a
010Mak <96219805+010Mak@users.noreply.github.com>
2023-09-07 13:19:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,6 @@
-from colorama import init, Fore
-
-def draw_dodecahedron():
-    print(Fore.MAGENTA)  # Set color to purple
-
-    # A more complex attempt to draw a simplified dodecahedron with interior lines
-    # Using "*" as a representation of vertices and "-" and "|" as edges
-    print("""
-          * * * *
-      *---------------*
-    /  .           .    \\
-  /  / \\         / \\    \\
-*--*---*---------*---*--*
-| \\    |        |    /  |
-|  \\   *--------*   /   |
-|   \\ /          \\ /    |
-|    *------------*     |
-|   / \\          / \\    |
-|  /   *--------*   \\   |
-| /    |        |    \\  |
-*--*---*---------*---*--*
-  \\  / \\       / \\  /
-    \\   '     '    /
-      *-----------*
-          * * * *
-    """)
+from colorama import init
+from image import draw_dodecahedron 
 
 if __name__ == "__main__":
-    init()  # Initialize the colorama
+    init()
     draw_dodecahedron()
