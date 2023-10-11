753ea9b65fbd480df952a85e898a747aa1cfce24
010Mak <96219805+010Mak@users.noreply.github.com>
2023-10-11 15:52:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import time
-from colorama import init, Fore, Back, Style
+import os
 from math import sin, cos, pi
-
-init(autoreset=True)
 
 def rainbow_gradient(n, phase=0, cycles=1):
     """Generate n colors as a gradient in the format of RGB based on phase."""
@@ -23,42 +21,22 @@ def print_colorful_artwork(artwork, colors):
     """Print colorful ASCII artwork."""
     for i, line in enumerate(artwork):
         for j, char in enumerate(line):
-            if char != ' ':  # Only color the non-space characters
-                print(rgb_to_ansi(*colors[(i+j) % len(colors)]) + char, end='', flush=True)
-            else:
-                print(char, end='')
+            print(rgb_to_ansi(*colors[(i+j) % len(colors)]) + char, end='', flush=True)
         print()
 
 if __name__ == "__main__":
-    # Define the ASCII square artwork
-    square_art = [
-        "#####",
-        "#   #",
-        "#   #",
-        "#   #",
-        "#####"
-    ]
+    # Read the ASCII artwork from the file
+    with open("artwork.txt", "r") as file:
+        artwork = file.readlines()
 
     phase = 0
     try:
         while True:
+            os.system('cls' if os.name == 'nt' else 'clear')  # Clear the console
             # Increment the phase to give appearance of shifting colors
             phase += pi / 30
-            colors = rainbow_gradient(10, phase, 2)  # 10 colors to cycle through for the square
-            print_colorful_artwork(square_art, colors)
+            colors = rainbow_gradient(max(len(line) for line in artwork) + len(artwork), phase, 2)
+            print_colorful_artwork(artwork, colors)
             time.sleep(0.1)  # Adjust this for faster/slower shifting of colors
-            # Clear the console to redraw the square
-            print('\033c', end='')  
     except KeyboardInterrupt:
         pass
-
-    choice = input("Would you like to use a custom word? (y/n): ").strip().lower()
-    while choice == 'y':
-        word = input("Enter the word: ")
-        speed = float(input("Enter the speed (e.g., 0.5): "))
-        colors = rainbow_gradient(len(word))
-        for char in word:
-            print(rgb_to_ansi(*colors[word.index(char) % len(colors)]) + char, end='', flush=True)
-            time.sleep(speed)
-        print(Style.RESET_ALL)  # Reset the style and move to the next line
-        choice = input("Would you like to use another custom word? (y/n): ").strip().lower()
