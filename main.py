027a56e0676e4f24cd12a8d0dfda4de12550832c
010Mak <96219805+010Mak@users.noreply.github.com>
2023-10-11 13:32:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import time
 from colorama import init, Fore, Back, Style
-from math import sin, cos
+from math import sin, cos, pi
 
 init(autoreset=True)
 
-def rainbow_gradient(n, cycles=1):
-    """Generate n colors as a gradient in the format of RGB."""
+def rainbow_gradient(n, phase=0, cycles=1):
+    """Generate n colors as a gradient in the format of RGB based on phase."""
     colors = []
     for i in range(n):
-        angle = (i / n) * 2 * 3.1415926 * cycles
+        angle = (i / n) * 2 * pi * cycles + phase
         r = int((1 + (0.5 * (1 + -sin(angle))) * -1) * 255)
         g = int((0.5 * (1 + cos(angle))) * 255)
         b = int((1 - (0.5 * (1 + sin(angle))) ** 2) * 255)
@@ -19,13 +19,12 @@ def rgb_to_ansi(r, g, b, background=False):
     """Convert RGB to an ANSI color code."""
     return f'\033[{48 if background else 38};2;{r};{g};{b}m'
 
-def print_colorful_artwork(artwork, colors, speed):
+def print_colorful_artwork(artwork, colors):
     """Print colorful ASCII artwork."""
     for i, line in enumerate(artwork):
         for j, char in enumerate(line):
             if char != ' ':  # Only color the non-space characters
                 print(rgb_to_ansi(*colors[(i+j) % len(colors)]) + char, end='', flush=True)
-                time.sleep(speed)
             else:
                 print(char, end='')
         print()
@@ -40,9 +39,18 @@ if __name__ == "__main__":
         "#####"
     ]
 
-    # Print default colorful square first
-    colors = rainbow_gradient(10, 2)  # 10 colors to cycle through for the square
-    print_colorful_artwork(square_art, colors, 0.05)  # Using 0.05 seconds speed for the square
+    phase = 0
+    try:
+        while True:
+            # Increment the phase to give appearance of shifting colors
+            phase += pi / 30
+            colors = rainbow_gradient(10, phase, 2)  # 10 colors to cycle through for the square
+            print_colorful_artwork(square_art, colors)
+            time.sleep(0.1)  # Adjust this for faster/slower shifting of colors
+            # Clear the console to redraw the square
+            print('\033c', end='')  
+    except KeyboardInterrupt:
+        pass
 
     choice = input("Would you like to use a custom word? (y/n): ").strip().lower()
     while choice == 'y':
